e36d6e62d1c6e4d1694df62c83a4d9468dfff559
CBreakr <github email address>
2020-06-15 01:22:15 -0400

    first round of stylistic changes

--- src/Containers/UserActionContainer.js
+++ src/Containers/UserActionContainer.js
@@ -6,17 +6,10 @@ import SignupForm from "../Components/SignupForm";
 class UserActionContainer extends React.Component {
     render(){
         return (
-            <>
-            <div>
-                <div className="main-title">Tetris</div>
-                <div className="user-action-container">
-                    <LoginForm setCurrentUser={this.props.setCurrentUser} />          
-                        {/* <div className='border-line'></div>
-                    */}
-                    <SignupForm setCurrentUser={this.props.setCurrentUser} />
-                </div>
+            <div className="user-action-container">                
+                <LoginForm setCurrentUser={this.props.setCurrentUser} />          
+                <SignupForm setCurrentUser={this.props.setCurrentUser} />
             </div>
-            </>
         )
     }
 }
